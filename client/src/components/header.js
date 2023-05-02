34351cda35cb92ae8a9d6a7aa529bd736120818e
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-05-02 09:56:47 -0600

    idk

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Header = () => {
+return (
+    <div>
+        
+    </div>
+);    
+}
