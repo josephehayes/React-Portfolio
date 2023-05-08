ad9b0c0da74d1dc8caa9e15df85bb25ec287fec8
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-05-08 15:23:31 -0600

    Updated Header component with new structure

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box'
 
 const Header = () => {
-return (
-    <div>
-        
-    </div>
-);    
+    return (
+        <AppBar>
+            <Toolbar>
+                Logo
+            </Toolbar>
+            <Tabs>
+                <Tab label='About Me'/>
+                <Tab label='Projects'/>
+                <Tab label='Contact'/>
+            </Tabs>
+        </AppBar>
+    );
 }
+
+export default Header;
