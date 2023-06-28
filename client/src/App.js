f7c3d8610e1cd538d62a54f70ceec467d81895b2
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-06-28 14:23:07 -0600

    Restructure files, adjusted theme, built extra components

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,50 @@ import './App.css';
 import React from 'react';
 // eslint-disable-next-line
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import theme from "./styles/index";
 import Header from './components/header';
+// eslint-disable-next-line
+import Home from './components/home';
+// eslint-disable-next-line
+import { Container } from '@mui/material';
 
 function App() {
   return (
-    <html class=''>
-      <div class=''>
-        <Header/>
-        <div class=''>
-        <h1 className="">
-          Hello world!
-        </h1>
-        </div>
-      </div>
-    </html>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header />
+        <Tabs centered={true}>
+          <Tab sx={{ mx: 'auto', my: 'auto' }} label='About Me' />
+          <Tab sx={{ mx: 'auto', my: 'auto' }} label='Projects' />
+          <Tab sx={{ mx: 'auto', my: 'auto' }} label='Contact' />
+        </Tabs>
+        {/*
+           <Route
+           path="/"
+           element={<Home />} 
+           />
+           <Route
+           path="/projects"
+           element={<Projects />} 
+           />
+           <Route
+           path="/"
+           element={<Contact />} 
+           />
+           
+          */}
+
+
+        {/*
+        <Footer />
+         */}
+
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Link } from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { Box, Typography, createTheme } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 
 const Header = () => {
     return (
         <AppBar>
             <Box>
-            <Toolbar sx={{ my: 3, mx: 3 }}>
-                <Typography component='h1' variant='h3'>
-                Joseph Hayes||
-                </Typography>
-            </Toolbar>
-            <Tabs centered={true}>
-                <Tab sx={{ mx: 'auto', my: 'auto' }} label='About Me'/>
-                <Tab sx={{ mx: 'auto', my: 'auto' }} label='Projects'/>
-                <Tab sx={{ mx: 'auto', my: 'auto' }} label='Contact'/>
-            </Tabs>
+                <Toolbar sx={{ my: 3, mx: 3 }}>
+                    <Typography component='h1' variant='h3'>
+                        Joseph Hayes||
+                        Full-stack Developer
+                    </Typography>
+                </Toolbar>
             </Box>
         </AppBar>
     );
