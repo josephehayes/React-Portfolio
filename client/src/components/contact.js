802017be7a3a2805f5faef4b767b378d91bc6697
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-07-24 19:05:34 -0600

    Adjust icon sizes, spacing, layout

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,7 +1,6 @@
 // es-lint disable next line
 import { Box, Grid, Stack, Link, Typography } from '@mui/material';
 import { LinkedIn, Twitter, GitHub } from '@mui/icons-material';
-import theme from '../styles';
 
 import React from 'react';
 
@@ -10,36 +9,38 @@ const Contact = () => {
         <Grid container
             orientation={'vertical'}
             justifyContent={'center'}
-            rowSpacing={4}
         >
             <Grid item spacing={4} xs={12} alignItems={'center'}>
                 <Box sx={{
-                    maxWidth: '33%',
+                    display: 'flex',
+                    maxWidth: '25%',
                     marginLeft: 'auto',
                     marginRight: 'auto',
-                    textAlign: 'center',
-                    textJustify: 'space-around'
+                    justifyContent: 'space-around',
+                    
                 }}>
-                    <Link href="https://github.com/josephehayes" classes={theme.icon} target="_blank" >
-                        <GitHub />
+                    <Link href="https://github.com/josephehayes" target="_blank" >
+                        <GitHub fontSize='large' />
                     </Link>
                     <Link href="https://www.linkedin.com/in/josephehayes1/" target="_blank" >
-                        <LinkedIn />
+                        <LinkedIn fontSize='large' />
                     </Link>
                     <Link href="https://twitter.com/s3f96" target="_blank" >
-                        <Twitter />
+                        <Twitter fontSize='large' />
                     </Link>
                     {/* Filler */}
                 </Box>
             </Grid>
             <Grid item>
                 <Stack>
-                    {/* 2 input fields (Your Email, Your Message)
+                    <Typography variant="body1" color="">
+                        {/* 2 input fields (Your Email, Your Message)
                     , 1 button (Send Message)
                     Will send to me somehow, need to figure out a way */}
 
-                    {/* Filler Text */}
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit ut aliquam. Pellentesque diam volutpat commodo sed egestas egestas. Eget mauris pharetra et ultrices neque ornare aenean euismod. In mollis nunc sed id semper risus in hendrerit. Felis donec et odio pellentesque diam volutpat commodo sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Rhoncus urna neque viverra justo nec ultrices dui. A cras semper auctor neque vitae tempus quam pellentesque nec. Felis imperdiet proin fermentum leo vel orci porta non. Consectetur lorem donec massa sapien faucibus et molestie ac feugiat.
+                        {/* Filler Text */}
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit ut aliquam. Pellentesque diam volutpat commodo sed egestas egestas. Eget mauris pharetra et ultrices neque ornare aenean euismod. In mollis nunc sed id semper risus in hendrerit. Felis donec et odio pellentesque diam volutpat commodo sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Rhoncus urna neque viverra justo nec ultrices dui. A cras semper auctor neque vitae tempus quam pellentesque nec. Felis imperdiet proin fermentum leo vel orci porta non. Consectetur lorem donec massa sapien faucibus et molestie ac feugiat.
+                    </Typography>
                 </Stack>
             </Grid>
         </Grid>
